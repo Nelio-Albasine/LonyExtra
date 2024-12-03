0c9e6ffcc4a5eba44a24f354ca85430adc6e59d8
Nelio Albasine <nelioalbasineofficial@gmail.com>
2024-12-03 09:52:33 +0200

    commit apos ter feito a confuguracao do actions e a renomeacao dos diretorios, quase que primeiro commit no develpment

--- 0/Api/Access/OTP/VerifyOTP_and_CreateUser.php
+++ 0/Api/Access/OTP/VerifyOTP_and_CreateUser.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $output = [
                 'success' => true,
                 'message' => "Parabéns, sua conta foi criada com sucesso!",
-                'redirectTo' => "http://127.0.0.1:5500/0/Dashboard"
+                'redirectTo' => "http://127.0.0.1:5500/0/dashboard"
             ];
         } else {
             $output = [
