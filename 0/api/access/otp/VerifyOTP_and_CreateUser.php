4a4d4fe0e733c6d2cbd17a31d548122df212187b
Nelio Albasine <nelioalbasineofficial@gmail.com>
2024-12-04 14:43:37 +0200

    Backup apos ter corrrigido o dir dos logs no arquivo verify otp

--- 0/api/access/otp/VerifyOTP_and_CreateUser.php
+++ 0/api/access/otp/VerifyOTP_and_CreateUser.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('error_log', __DIR__ . '/../logs/verify_OTP.log');
+ini_set('error_log', __DIR__ . '/../../logs/verify_OTP.log');
 
 // Definindo os cabeçalhos CORS diretamente 
 header('Access-Control-Allow-Origin: *');
