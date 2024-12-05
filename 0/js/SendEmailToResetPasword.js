5544b0e58dc90421959a6dade9fa3bdbbeebdbf5
Nelio Albasine <nelioalbasineofficial@gmail.com>
2024-12-05 23:56:36 +0200

    NNN

--- 0/js/SendEmailToResetPasword.js
+++ 0/js/SendEmailToResetPasword.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     let btnResetPassword = document.getElementById("btnResetPassword")
     let inputEmailToResetPassword = document.getElementById("inputEmailToResetPassword")
 
+    let params = new URLSearchParams(window.location.search);
+
+    if (params.has("email")) {
+        let email = params.get("email");  
+        inputEmailToResetPassword.value = email;  
+    }
+    
+
     btnResetPassword.addEventListener("click", async (event) => {
         event.preventDefault();
         let value = inputEmailToResetPassword.value.trim();
 
         if (!value) {
-            alert( "Por favor, insira o email que você usou ao criar sua conta.");
+            alert("Por favor, insira o email que você usou ao criar sua conta.");
             return;
         }
 
@@ -16,6 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-async function tryToSendEmailToResetPassword(email){
+async function tryToSendEmailToResetPassword(email) {
 
 }
