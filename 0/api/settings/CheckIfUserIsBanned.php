6c54fea6cefa3629fef3d4bfd584c4cefaf06c2d
Nelio Albasine <nelioalbasineofficial@gmail.com>
2024-12-19 00:44:14 +0200

    Deploy debug conta bloqueada para user errados: v1.2

--- 0/api/settings/CheckIfUserIsBanned.php
+++ 0/api/settings/CheckIfUserIsBanned.php
@@ -1,18 +1,17 @@
 <?php
 function checkIfUserIsBanned($conn, $userId): bool
 {
+    //checkIfUserIsBanned
     $sql = "SELECT userLockStatus FROM Usuarios WHERE userId = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $userId);
+    $stmt->bind_param("s", $userId);
     $stmt->execute();
     $result = $stmt->get_result();
     $stmt->close();
-
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        return isset($row['userLockStatus']) && $row['userLockStatus'] === 1;
+        return $row['userLockStatus'] == 1;
     } else {
         return false;
     }
 }
-
